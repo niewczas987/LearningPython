c98df5432f882af338e0046c8245424923f0c834
KN <niewczas987@gmail.com>
2021-09-29 17:12:52 +0200

    Python book part 3 - exercises

---
+++ PythonBook_Part4/BasicsOfFunctions.py
@@ -0,0 +1,12 @@
+# pierwszy przykad
+def times(x, y):  # utworzenie i przypisanie funkcji
+    return x * y  # ciało funkcji wykonywane po wywołaniu
+
+
+print(times(3, 8))  # wwołanie funkcji z argumentami
+print(times('xD', 10))  # argumenty mogą być dowolne, bo polimorfizm przy *
+
+
+def intersect(seq1, seq2):
+    pass
+
