f35733cd2bcba8f88a34d556454260d386b16351
KN <niewczas987@gmail.com>
2021-10-07 12:28:51 +0200

    Python book part 5 - modules

---
+++ PythonBook_Part5_Modules/Misc.py
@@ -0,0 +1,16 @@
+"""
+import sys
+print(sys.path)
+"""
+
+
+'''
+creating .pth file
+'''
+# site_packages_path is the packages folder, which in my case is:
+site_packages_path = r'C:\Users\PLKANIE3\Documents\KN\Python\site_packages'
+# path that you wanna add, which again in my case is
+path_to_add = r'C:\Users\PLKANIE3\PycharmProjects\LearningPython\PythonBook_Part1'
+
+f = open(site_packages_path + '\custom_path.pth', 'a')
+f.write(path_to_add)
