2d57e558ffb4f9c3664824324e0eaab494933257
KN <niewczas987@gmail.com>
2022-01-03 16:14:48 +0100

    Python text editor - final touches

---
+++ TkinterExample/ChatApp/test_files/scrollbar_for_whole_window.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+
+win = tk.Tk()
+canvas = tk.Canvas(win)
+frame = tk.Frame(canvas)
+
+scroll = tk.Scrollbar(win, orient='vertical', command=canvas.yview)
+scroll.pack(side=tk.RIGHT, fill=tk.Y)
+
+canvas.configure(yscrollcommand=scroll.set)
+canvas.pack(fill=tk.BOTH, expand=1)
+canvas.create_window((0,0), window=frame, anchor='nw')
+
+for _ in range(30):
+    tk.Label(win, text='big label').pack(pady=20)
+
+win.mainloop()
